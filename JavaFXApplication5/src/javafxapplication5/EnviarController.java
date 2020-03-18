82afa9351769a6bd78604e43e35dbe93acd8cfef
Jeremy Reyes <jralboc@hotmail.com>
2020-03-18 15:58:36 -0300

    cambio escena

---
+++ JavaFXApplication5/src/javafxapplication5/EnviarController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication5;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author jralb
+ */
+public class EnviarController implements Initializable {
+    
+    @FXML
+    private AnchorPane root;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- JavaFXApplication5/src/javafxapplication5/FXMLDocumentController.java
+++ JavaFXApplication5/src/javafxapplication5/FXMLDocumentController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -19,13 +21,49 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private AnchorPane root;
     
     @FXML
+    private Label label;
+     
+    @FXML
+    private Button bot1;
+        
+    @FXML
+    private Button bot2;
+    
+    @FXML
+    private Button bot3;
+        
+    @FXML
+    private Button bot4;
+    
+        
+    @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        
     }
+    
+    @FXML
+    private void enviar(ActionEvent event) {
+        SceneHandler.cargarVista(root, getClass().getResource("Enviar.fxml"));  
+    } 
+    
+    @FXML
+    private void historial(ActionEvent event) {
+        SceneHandler.cargarVista(root, getClass().getResource("FXMLDocument.fxml"));  
+    } 
+    
+    @FXML
+    private void responder(ActionEvent event) {
+        SceneHandler.cargarVista(root, getClass().getResource("ModificarPlayListGeneral.fxml"));  
+    } 
+    
+    @FXML
+    private void horaactual(ActionEvent event) {
+        SceneHandler.cargarVista(root, getClass().getResource("ModificarPlayListGeneral.fxml"));  
+    } 
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
